d750e59558b647bd7d726af13e6f01c1a978f9da
SACHSACH <kamos6597@gmail.com>
2024-02-04 18:53:57 +0300

    initial commit

---
+++ 04-store-api/starter/controllers/products.js
@@ -0,0 +1,16 @@
+const Product = require('../models/product')
+
+const getAllProductsStatic = async (req,res) =>{
+    // throw new Error('testing async errors')
+    const products = await Product.find({})
+    res.status(200).json({products})
+}
+
+const getAllProducts = async (req,res) =>{
+    res.status(200).json({msg: 'products route'})
+}
+
+module.exports = {
+    getAllProducts, 
+    getAllProductsStatic
+}
